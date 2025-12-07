1d8adfbd9655c75769c9fa8f0fcdc66da1e505c2
Esteban Quiroga <eaquiroga@espe.edu.ec>
2025-12-07 09:49:18 -0500

    PRY10_MEDIAQUERIES

---
+++ SEGUNDO_PARCIAL/PRY_INTRODUCCION_JS/public/js/main.js
@@ -0,0 +1,14 @@
+//declarar info de usuario
+
+let nombre = "Juan";
+let apellido = "Perez";
+let edad = 28;
+let peso = 75.5;
+let ciudad = "Quito";
+
+let nombreCompleto = nombre + " " + apellido;
+
+
+console.log("Nombre Completo: " + nombreCompleto);  
+
+console.log(`Mi nombre es ${nombreCompleto}, tengo ${edad} años, vivo en ${ciudad}, y peso ${peso} kg.`);
